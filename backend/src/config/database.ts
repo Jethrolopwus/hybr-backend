b3e10d73a87da4ec8b38f8e16f7aeaccad444011
lopwusjethro92@gmail.com <lopwusjethro92@gmail.com>
2025-04-04 17:57:07 +0100

    remove .env

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -1,29 +1,29 @@
-import { Sequelize } from 'sequelize';
-import dotenv from 'dotenv';
+import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
 
 dotenv.config();
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME || 'hybr_toolkit',
-  process.env.DB_USER || 'hybr_user',
-  process.env.DB_PASS || 'Jethsmith@25',
+  process.env.DB_NAME || "hybr_toolkit",
+  process.env.DB_USER || "hybr_user",
+  process.env.DB_PASS || "NVkjMWJrdMvy8Laa5iK7j3i9LJuZqmcV",
   {
-    host: process.env.DB_HOST || 'localhost',
-    port: parseInt(process.env.DB_PORT || '5432'),
-    dialect: 'postgres',
+    host: process.env.DB_HOST || "dpg-cvo0joemcj7s73b1d360-a",
+    port: parseInt(process.env.DB_PORT || "5432"),
+    dialect: "postgres",
     dialectOptions: {
-      ssl: false
+      ssl: false,
     },
     define: {
-      timestamps: false
+      timestamps: false,
     },
     pool: {
       max: 5,
       min: 0,
       acquire: 30000,
-      idle: 10000
+      idle: 10000,
     },
-    logging: console.log
+    logging: console.log,
   }
 );
 
